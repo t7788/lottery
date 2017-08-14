fbdbf8821b736488423f92eda2c77b0f84b9d90a
soonjone <soonjone@qq.com>
2017-08-14 14:31:50 +0800

    mod

--- schedule/getodds.js
+++ schedule/getodds.js
@@ -2,36 +2,37 @@
 var schedule = require('node-schedule')
 var superagent = require('superagent')
 var moment = require('moment')
+var request = require('request')
 
-schedule.scheduleJob('0 */5 * * * *', function () {
+schedule.scheduleJob('0 */10 * * * *', function () {
     let day = moment().format('YYYY-MM-DD')
     let time = moment().format('YYYY-MM-DD HH:mm:ss')
     console.log(time)
     let url = "http://i.sporttery.cn/odds_calculator/get_odds?i_format=json&i_callback=&poolcode[]=had&poolcode[]=hhad&poolcode[]=ttg&poolcode[]=crs&poolcode[]=hafu"
-    superagent.get(url)
-        .set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.13 Safari/537.36')
-        .end(function (err, sres) {
-            console.log('superagent')
-            if (err) {
-                console.log(err)
-                return
-            }
-            console.log('sss')
-            let jsonObj = JSON.parse(sres.text)
-            console.log(jsonObj.status)
+
+    var options = {
+        url: url,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.13 Safari/537.36'
+        }
+    };
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let jsonObj = JSON.parse(body)
             if (jsonObj.status.last_updated) {
                 let Redis = require('ioredis');
                 let redis = new Redis(6001, '122.226.180.195')
-                redis.set("odds_" + day, sres.text).then(function (result) {
-                    console.log(result)
-                    redis.quit()
-                })
-                console.log("fail")
-
+                redis.set("odds_" + day, body)
+                redis.quit()
+                console.log('succ')
             } else {
                 console.log(jsonObj.status)
             }
-        })
+        } else {
+            console.log(error)
+        }
+    })
+
 })
 
 

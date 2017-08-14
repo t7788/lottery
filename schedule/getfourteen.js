eb4f5c25f1169c32e7e71c52168c80621075c78d
soonjone <soonjone@qq.com>
2017-08-14 11:19:44 +0800

    mod

--- schedule/getfourteen.js
+++ schedule/getfourteen.js
@@ -3,8 +3,9 @@ var schedule = require('node-schedule')
 var superagent = require('superagent')
 var moment = require('moment')
 var cheerio = require('cheerio')
+var request = require('request')
 
-schedule.scheduleJob('0 0 */2 * * *', function () {
+schedule.scheduleJob('0 */30 * * * *', function () {
     let time = moment().format('YYYY-MM-DD HH:mm:ss')
     console.log(time)
     superagent.get('http://trade.500.com/sfc/')
@@ -18,27 +19,49 @@ schedule.scheduleJob('0 0 */2 * * *', function () {
             let period = $('#expect_tab a').eq(0).attr('data-val')
             console.log(period)
             if (period) {
-                let url = "http://i.sporttery.cn/wap/fb_lottery/fb_lottery_match?key=wilo&num=" + period
-                superagent.get(url)
-                    .end(function (err, sres) {
-                        if (err) {
-                            console.log(err)
-                            return
-                        }
+                period = parseInt(period)
+                let Redis = require('ioredis');
+                let redis = new Redis(6001, '122.226.180.195')
+                let count = 0
+                let items = []
+                redis.set("period", period)
 
-                        let jsonObj = JSON.parse(sres.text)
-                        let Redis = require('ioredis');
-                        let redis = new Redis(6001, '122.226.180.195')
-                        redis.set("period", period)
-                        if (jsonObj.status.code == 0) {
-                            redis.set("fourteen_" + period, sres.text).then(function (result) {
-                                console.log(result)
-                                redis.quit()
-                            })
+                let periods = [period - 1, period]
+                console.log(periods)
+                for (let i = 0; i < 2; i++) {
+                    let url = "http://i.sporttery.cn/wap/fb_lottery/fb_lottery_match?key=wilo&num=" + periods[i]
+                    var options = {
+                        url: url,
+                        headers: {
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.13 Safari/537.36'
+                        }
+                    };
+                    request(options, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            let jsonObj = JSON.parse(body)
+                            if (jsonObj.status.code == 0) {
+                                items.push({key: "fourteen_" + periods[i], info: body})
+                                count++
+                                endQuery()
+                            } else {
+                                console.log(jsonObj.status)
+                            }
                         } else {
-                            console.log(jsonObj.status)
+                            console.log(error)
                         }
                     })
+                }
+
+                function endQuery() {
+                    if (count == 2) {
+                        items.forEach(function (item) {
+                            redis.set(item.key, item.info).then(function (result) {
+                                console.log(result)
+                            })
+                        })
+                        redis.quit()
+                    }
+                }
             }
         })
 })
--- test.js
+++ test.js
@@ -4,49 +4,66 @@ var express = require('express')
 var moment = require('moment')
 
 var app = express()
+var cheerio = require('cheerio')
 var superagent = require('superagent')
 
-/*let redis = require("redis")
-let client = redis.createClient(6001, '122.226.180.195', {});
+/*const https = require('https');
 
-client.on("error", function (err) {
-    console.log("Error " + err);
+var options = {
+    hostname: 'myportal.vtc.edu.hk',
+    port: 443,
+    path: '/wps/portal',
+    method: 'GET',
+    secureProtocol: 'TLSv1_method'
+};
+
+var req = https.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
 });
-var period = ''
-client.get("period", function (err, reply) {
-    period = reply
-    console.log(period)
-    client.get("deadline_" + period, function (err, reply) {
-        console.log(reply)
-        client.quit()
-    })
+req.end();
+
+req.on('error', (e) => {
+    console.error(e);
+});*/
+
+/*var http = require('http');
+var querystring = require('querystring');
+var options = {
+    host: 'http://i.sporttery.cn/wap/fb_lottery/fb_lottery_match?key=wilo&num=17113', // 这个不用说了, 请求地址
+    port: 80,
+    path: '/', // 具体路径, 必须以'/'开头, 是相对于host而言的
+    method: 'GET', // 请求方式, 这里以post为例
+    headers: { // 必选信息, 如果不知道哪些信息是必须的, 建议用抓包工具看一下, 都写上也无妨...
+        'Content-Type': 'application/json'
+    }
+};
+http.get(options, function (res) {
+    var resData = "";
+    res.on("data", function (data) {
+        resData += data;
+    });
+    res.on("end", function () {
+        console.log(resData)
+    });
 })*/
 
-superagent.get('http://www.baidu.com')
-    .end(function (err, sres) {
+var request = require('request');
+request('http://i.sporttery.cn/wap/fb_lottery/fb_lottery_match?key=wilo&num=17113', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        //console.log(body)
 
-        if (err) {
-            console.log(err.status)
-            return
-        }
+       /* console.log(error)
+        let $ = cheerio.load(body)
+        let deadline = $('.overTime em').text().trim()*/
+        console.log(body)
 
-        console.log('not')
-    })
-
-
-/*
-redis.get('period').then(function (result) {
-    console.log(result)
-    return redis.get("deadline_" + result)
-}).then(function (result) {
-    console.log(result)
-    return redis.get('period')
-}).then(function (result) {
-    console.log(result)
-    redis.quit()
+    }
 })
-*/
-
 
 
 

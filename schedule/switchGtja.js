d408fc5cf882a8778bdde414f6996a8c3487318b
zsjun <t7@qq.com>
2017-08-23 17:37:25 +0800

    ss

--- schedule/switchGtja.js
+++ schedule/switchGtja.js
@@ -1,7 +1,10 @@
 // 引入依赖
 var schedule = require('node-schedule')
+var moment = require('moment')
 
 schedule.scheduleJob('0 0 7 * * *', function () {
+    let time = moment().format('YYYY-MM-DD HH:mm:ss')
+    console.log(time)
     let Redis = require('ioredis');
     let redis = new Redis(6001, '122.226.180.195')
     redis.set("gtja_start", 1)
@@ -9,6 +12,8 @@ schedule.scheduleJob('0 0 7 * * *', function () {
 })
 
 schedule.scheduleJob('0 0 21 * * *', function () {
+    let time = moment().format('YYYY-MM-DD HH:mm:ss')
+    console.log(time)
     let Redis = require('ioredis');
     let redis = new Redis(6001, '122.226.180.195')
     redis.set("gtja_start", 0)

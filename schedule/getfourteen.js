39da7a9a0560982ce06d50d4f5a6c04f3187f7f4
zsjun <t7@qq.com>
2017-09-20 13:28:38 +0800

    ss

--- schedule/getfourteen.js
+++ schedule/getfourteen.js
@@ -4,10 +4,13 @@ var superagent = require('superagent')
 var moment = require('moment')
 var cheerio = require('cheerio')
 var request = require('request')
+var http = require('http')
+
 
 schedule.scheduleJob('0 */30 * * * *', function () {
     let time = moment().format('YYYY-MM-DD HH:mm:ss')
     console.log(time)
+
     superagent.get('http://trade.500.com/sfc/')
         .end(function (err, sres) {
 
@@ -33,10 +36,14 @@ schedule.scheduleJob('0 */30 * * * *', function () {
                     var options = {
                         url: url,
                         headers: {
-                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.13 Safari/537.36'
+                            'Host': 'i.sporttery.cn',
+                            'Cookie': 'COLLCK=1186171230; Hm_lvt_860f3361e3ed9c994816101d37900758=1503048846; COLLCK=1186171230',
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0'
                         }
-                    };
+                    }
+
                     request(options, function (error, response, body) {
+                        console.log(body)
                         if (!error && response.statusCode == 200) {
                             let jsonObj = JSON.parse(body)
                             if (jsonObj.status.code == 0) {
